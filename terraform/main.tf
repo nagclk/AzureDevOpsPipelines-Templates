c441b3e4dd404260889c11cbcaf99498da69181b
nagclk <nagclk@gmail.com>
2025-07-28 23:39:13 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,7 +6,7 @@ provider "azurerm" {
 
 resource "azurerm_resource_group" "rg" {
   name     = "${var.resource-group-name}-${var.environment}"
-  location = yamldecode(file("azure-pipelines.yml"))["location"]
+  location = yamldecode(file("../azure-pipelines.yml"))["location"]
   
   tags = {
     environment = var.environment
