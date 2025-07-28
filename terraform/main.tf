46228e92ed94c259d867fb2ddc970418df60ca87
nagclk <nagclk@gmail.com>
2025-07-28 23:51:59 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,7 +6,7 @@ provider "azurerm" {
 
 resource "azurerm_resource_group" "rg" {
   name     = "${var.resource-group-name}-${var.environment}"
-  location = local.config.location
+  location = local.config
   
   tags = {
     environment = var.environment
